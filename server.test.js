d95fef31513855afa8672338c91eea1161a9d76b
Usman Babar <usmanbabar1909@gmail.com>
2022-11-22 14:15:19 +0500

    first commit

--- server.test.js
+++ server.test.js
@@ -1,6 +1,8 @@
 const request = require("supertest");
 const app = require("./routes/User");
 const mongoose = require("mongoose");
+const { connect } = require("superagent");
+const { config } = require("dotenv");
 
 describe("Test The Root Path", () => {
   test("It should response the POST method", () => {
@@ -10,22 +12,23 @@ describe("Test The Root Path", () => {
       Age: 12,
       Contact: 123,
     });
-    expect(response.statusCode).toBe(undefined);
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty("post");
   });
 });
 
 describe("Test The Delete By ID", () => {
   test("It should response the DELETE method", () => {
     const response = request(app).delete("http://localhost:8000/User/:id");
-    expect(response.statusCode).toBe(undefined);
+    expect(response.statusCode).toBe(200);
   });
 });
 
 describe("Test The Root Path", () => {
   test("It should response the POST method", () => {
     const response = request(app).post("http://localhost:8000/User/add").send({
-      Name: "Obaid",
+      Name: "Usman",
     });
-    expect(response.statusCode).toBe(undefined);
+    expect(response.body.toEqual("Usman").statusCode).toBe(200);
   });
 });
